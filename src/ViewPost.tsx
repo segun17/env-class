65f01ebf18ff1d68fb7d564ea417ca2745689ed0
segun oladokun <segunoladokun47@gmail.com>
2025-04-24 18:26:12 -0700

    i deleted the app6

--- src/ViewPost.tsx
+++ src/ViewPost.tsx
@@ -7,7 +7,7 @@
 //     const params= useParams()
 //     console.log(params.id)
 //     useEffect(()=>{
-//         axios.get(`https://jsonplaceholder.typicode.com/posts/${params.id}`).then((res)=>{
+        // axios.get(`https://jsonplaceholder.typicode.com/posts/${params.id}`).then((res)=>{
 //             console.log(res.data)
 //             setSinglePost(res.data)
 //         }).catch((err)=>{
@@ -25,34 +25,34 @@
 // export default ViewPost
 
 
-import axios from 'axios'
-import React, { useState } from 'react'
+// import axios from 'axios'
+// import React, { useState } from 'react'
 
-function ViewPost() {
-    const [city,setCity]=useState("")
-   const [data,setData]=useState({})
-    const handleWeather=()=>{
-       axios.get(`https://api.openweathermap.org/data/2.5/weather?q=London&appid=4ee1fb0f082747a8021bb5edffc440c5`).then((res)=>{
-         console.log(res)
-       }).catch((err)=>{
-        console.log(err)
-       })
-    }
+// function ViewPost() {
+//     const [city,setCity]=useState("")
+//    const [data,setData]=useState({})
+//     const handleWeather=()=>{
+//        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=London&appid=4ee1fb0f082747a8021bb5edffc440c5`).then((res)=>{
+//          console.log(res)
+//        }).catch((err)=>{
+//         console.log(err)
+//        })
+//     }
 
-  return (
-    <div>
-        <div>
-            <input className='border' value={city} onChange={(e)=>setCity(e.target.value)}/>
-            <button onClick={handleWeather} className='block bg-gray-200'>Check</button>
-        </div>
-        <div className='bg-gray-200 text-white p-3 rounded mt-4 space-y-5'>
-            <p>Name:</p>
-            <p>Desc:</p>
-            <p>Temp:</p>
-            <p>Hum: </p>
-        </div>
-    </div>
-  )
-}
+//   return (
+//     <div>
+//         <div>
+//             <input className='border' value={city} onChange={(e)=>setCity(e.target.value)}/>
+//             <button onClick={handleWeather} className='block bg-gray-200'>Check</button>
+//         </div>
+//         <div className='bg-gray-200 text-white p-3 rounded mt-4 space-y-5'>
+//             <p>Name:</p>
+//             <p>Desc:</p>
+//             <p>Temp:</p>
+//             <p>Hum: </p>
+//         </div>
+//     </div>
+//   )
+// }
 
-export default ViewPost
+// export default ViewPost
