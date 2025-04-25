64478bd841909a06b441d676f78128c3dda778f4
segun oladokun <segunoladokun47@gmail.com>
2025-04-24 17:48:21 -0700

    i commented some of the app

--- src/App1.tsx
+++ src/App1.tsx
@@ -1,22 +1,22 @@
-import axios from 'axios'
-import  { useEffect } from 'react'
+// import axios from 'axios'
+// import  { useEffect } from 'react'
 
-function App() {
-    const apikey= import.meta.env.VITE_backendApi
-    console.log(apikey)
+// function App() {
+//     const apikey= import.meta.env.VITE_backendApi
+//     console.log(apikey)
 
-    useEffect(()=>{
-        axios.get(`${import.meta.env.BASE_URL}${apikey}`).then((res)=>{
+//     useEffect(()=>{
+//         axios.get(`${import.meta.env.BASE_URL}${apikey}`).then((res)=>{
 
-        }).catch((err)=>{
-       console.log(err)
-        })
-    })
-  return (
-    <div>
-      Hello 
-    </div>
-  )
-}
+//         }).catch((err)=>{
+//        console.log(err)
+//         })
+//     })
+//   return (
+//     <div>
+//       Hello 
+//     </div>
+//   )
+// }
 
-export default App
+// export default App
